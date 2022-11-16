db954cdd2ca8a6d6bd02ad53dc44edee8bca2723
Murilll <murilosertorio25@gmail.com>
2022-11-16 11:52:07 -0300

    .-.

--- Ex.1/Program.cs
+++ Ex.1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+
+List<string> arr = new List<string>();
+
+int temp = 0;
+
+while (true)
+{
+    Console.WriteLine("Digite um caracter: ");
+    string num = Console.ReadLine();
+    arr.Add(num);
+    Console.WriteLine("Digite 0 para parar");
+    int stop = Convert.ToInt32(Console.ReadLine());
+
+    if (stop == 0)
+        break;
+    else
+        continue;
+}
+
+for (int write = 0; write < arr.Count; write++)
+{
+    for (int sort = 0; sort < arr.Count; sort++)
+    {
+        if (arr[sort] > arr[sort + 1])
+        {
+            temp = arr[sort + 1];
+            arr[sort + 1] = arr[sort];
+            arr[sort] = temp;
+        }
+    }
+}
+
+for (int i = 0; i < arr.length; i++)
+    Console.Write(arr[i] + " ");
+
+Console.ReadKey();
+
+Console.Write("Lista: ");
+foreach (var i in arr)
+{  
+    Console.Write($"{i}");
+}
--- Ex.2/Program.cs
+++ Ex.2/Program.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 
-List<int> notas = new List<int>();
+List<float> notas = new List<float>();
 int x = 0;
-for (int i = 0; i < 3; i++)
+for (int i = 0; i < 6; i++)
 {
-    x++;
-    Console.WriteLine($"Digite a {x}º nota do aluno: ");
+    
+    Console.WriteLine($"Digite a {x+1}º nota do aluno: ");
     int addNotas = Convert.ToInt32(Console.ReadLine());
     notas.Add(addNotas);
+    x++;
+}
+
+float media = notas.Sum() / notas.Count();
+
+Console.WriteLine(media);
+
+
+foreach (var item in notas)
+{
+    Console.Write(item + " ");
 }
