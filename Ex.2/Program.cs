d53f8841c37f1e8e55d40340cff2cf1530400a1e
Murilll <murilosertorio25@gmail.com>
2022-11-16 11:06:16 -0300

    .-.

---
+++ Ex.2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+List<int> notas = new List<int>();
+int x = 0;
+for (int i = 0; i < 3; i++)
+{
+    x++;
+    Console.WriteLine($"Digite a {x}º nota do aluno: ");
+    int addNotas = Convert.ToInt32(Console.ReadLine());
+    notas.Add(addNotas);
+}
